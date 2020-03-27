8982f086406b3816641be8c77513f27e4abfb758
Marcelo Santos <irsmarcello@gmail.com>
2020-03-27 17:57:20 +0000

    Novos arquivos2

---
+++ mod22.lua
@@ -0,0 +1,29 @@
+function data()
+return {
+	info = {
+		minorVersion = 0,
+		severityAdd = "NONE",
+		severityRemove = "NONE",
+		name = _("A380 Pack"),
+		description = _("A380 PACK Molanair"),
+		authors = {
+			{
+				name = "MJ1989C",
+				role = "creator",
+			},
+			{
+				name = "MJ1989C",
+				role = "textures",
+			},
+		},
+		tags = { "vehicle", "plane", "passenger", "Europe", "USA", "Airbus", "A380" },
+		
+		visible = true
+	},
+	options = {
+	},
+		runFn = function (settings)
+		
+	end
+}
+end
